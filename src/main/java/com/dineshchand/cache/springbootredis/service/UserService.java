95e9271292e7b3df16049d569259f0fb8ed3a213
SANJEEVSK005 <sanjeevsk0005@gmail.com>
2024-11-07 09:47:44 +0530

    add

---
+++ src/main/java/com/dineshchand/cache/springbootredis/service/UserService.java
@@ -0,0 +1,48 @@
+package com.dineshchand.cache.springbootredis.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import com.dineshchand.cache.springbootredis.model.User;
+import com.dineshchand.cache.springbootredis.repository.UserDatabaseRepo;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserDatabaseRepo userDatabaseRepo;
+
+	// @CachePut(value="users",key="#user.id")
+	//dont put in cache during add. Otherwise cache will be full
+	public User addUser(User user) {
+		userDatabaseRepo.save(user);
+		return user;
+	}
+
+	public Iterable<User> findAllUsers() {
+		return userDatabaseRepo.findAll();
+	}
+
+	@CacheEvict(value="users",key="#id")
+	public void deleteUser(String id) {
+		userDatabaseRepo.deleteById(id);;
+	}
+
+	@CachePut(value="users",key="#id")
+	public User updateUser(String id,User user) {
+		return userDatabaseRepo.save(user);
+	}
+
+	@Cacheable(value="users",key="#id")
+	public User findUser(String id) {
+		System.out.println("retrieving users from Database*********");
+		Optional<User> maybeUser =  userDatabaseRepo.findById(id);
+		return maybeUser.orElse(null);
+	}
+	
+}
---
+++ src/main/java/com/dineshchand/cache/springbootredis/web/RedisController.java
@@ -0,0 +1,59 @@
+package com.dineshchand.cache.springbootredis.web;
+
+import java.util.Optional;
+
+import com.dineshchand.cache.springbootredis.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dineshchand.cache.springbootredis.model.User;
+
+@RestController
+@RequestMapping("/redis")
+public class RedisController {
+	
+	@Autowired
+    private UserService userService;
+
+    @PostMapping("/adduser")
+    public User save(@RequestBody User user){
+        userService.addUser(user);
+        return user;
+    }
+
+    @GetMapping("/users")
+    public Iterable<User> list(){
+        return userService.findAllUsers();
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable String id){
+        return userService.findUser(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public User update(@RequestBody User user,@PathVariable String id){
+    	System.out.println("inside update");
+    	User oldUser = userService.findUser(id);
+    	oldUser.setName(user.getName());
+    	oldUser.setAge(user.getAge());
+        final User updatedUser = userService.updateUser(id, user);
+        return updatedUser;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteUser(@PathVariable String id){
+        userService.deleteUser(id);
+        return id;
+    }
+}
